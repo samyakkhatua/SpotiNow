bd50fc902eff075e6eec19498710aefe773d4183
samyakkhatua <samyakkhatua@duck.com>
2023-07-18 20:49:59 +0530

    refresh token API

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI, Request
+import multiprocessing
+from fastapi import FastAPI, Request, BackgroundTasks
 from fastapi.responses import RedirectResponse
 from fastapi.responses import JSONResponse
 from fastapi import HTTPException
@@ -10,8 +11,6 @@ import base64
 import secrets
 import string
 
-
-
 app = FastAPI()
 
 @app.get("/")
@@ -19,12 +18,14 @@ def read_root():
     return {"Welcome to Spotinow!"}
 
 SPOTIFY_GET_CURRENT_TRACK_URL = 'https://api.spotify.com/v1/me/player/currently-playing'
-access_token = "BQCfemif4Bjl-rgqyFrVg42AqWvWPX0MnbzRIWNlclfUqmqYvDDs7SXOVMTz-gITdT6UTjrSD-HOOnBn1F1Lig71R3TKIW74ciopJM3tpyOCxeOKdif9kHgfiII5xawV6T55XE9n0BFcXsKmKhfosmLO7_ImwcyZjYDY-JE7pFWrLCUVduxr2SwVvxLnVHzXszxd2JRLh2wACwDQUcgUpdWyu-sc"
+access_token = None
 refresh_token = None
+token_expires_at = None
 
 CLIENT_ID = '36f15c5c0d4f424ea5c8f23193a87ef6'
 CLIENT_SECRET = '04da3a43c83d4d8b8b2cce9f77398494'
-redirect_uri = 'http://127.0.0.1:8000/callback'
+# redirect_uri = 'http://127.0.0.1:8000/callback'
+redirect_uri = 'https://spotinow-1-t1281143.deta.app/callback'
 
 def generate_random_string(length: int) -> str:
     characters = string.ascii_letters + string.digits
@@ -46,6 +47,7 @@ def login():
 def callback(request: Request):
     global access_token
     global refresh_token
+    global token_expires_at
 
     code = request.query_params.get('code', None)
     state = request.query_params.get('state', None)
@@ -69,18 +71,13 @@ def callback(request: Request):
         data = response.json()
         access_token = data['access_token']
         refresh_token = data['refresh_token']
+        token_expires_at = time.time() + 3600  # Set default expiration time to 60 minutes (3600 seconds)
         return {'access_token': access_token, 'refresh_token': refresh_token}
 
-@app.get('/refresh_token')
-def refresh_token():
+def refresh_access_token():
     global access_token
-    global refresh_token
+    global token_expires_at
 
-    print(access_token)
-
-    if refresh_token is None:
-        return {'error': 'No refresh token available'}
-    
     auth_options = {
         'url': 'https://accounts.spotify.com/api/token',
         'data': {
@@ -91,17 +88,17 @@ def refresh_token():
         }
     }
 
+    print(access_token[-10:])
     response = requests.post(auth_options['url'], data=auth_options['data'])
     if response.status_code == 200:
         data = response.json()
         access_token = data['access_token']
-        print(access_token)
+        token_expires_at = time.time() + 3600  # Set default expiration time to 60 minutes (3600 seconds)
+        print(access_token[-10:])
         return {'access_token': access_token}
 
-# get current track 
 def get_current_track():
     global access_token
-    print(access_token)
 
     response = requests.get(
         SPOTIFY_GET_CURRENT_TRACK_URL,
@@ -111,7 +108,6 @@ def get_current_track():
     )
 
     json_resp = response.json()
-    print(json_resp)
 
     if response.status_code != 200:
         raise HTTPException(status_code=response.status_code, detail=json_resp.get('error'))
@@ -130,33 +126,27 @@ def get_current_track():
         "artists": artist_names,
         "link": link
     }
-
     return current_track_info
-
 
 @app.get("/currently-playing")
 def currently_playing():
     try:
         current_track_info = get_current_track()
-        return JSONResponse(content=current_track_info, status_code=200)
+        return current_track_info
     except HTTPException as e:
-        return JSONResponse(content={"error": str(e.detail)}, status_code=e.status_code)
+        return {"error": str(e.detail)}
 
+@app.get("/refresh-token")
+def refresh_token():
+    print("ok please")
+    try:
+        refreshed_token = refresh_access_token()
+        return refreshed_token
+    except Exception as e:
+        return {"error": str(e)}
 
 def main():
-    current_track_id = None
-    while True:
-        try:
-            current_track_info = get_current_track(access_token)
-
-            if current_track_info['id'] != current_track_id:
-                print(current_track_info)
-                current_track_id = current_track_info['id']
-
-            time.sleep(3)
-        except KeyboardInterrupt:
-            break
-
+    pass
 
 if __name__ == '__main__':
     main()
